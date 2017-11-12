829616dc6c0639b772f831a826ec11f222eb927c
Pedro Vasco <pedro.vasco1996@gmail.com>
2017-11-12 19:47:53 +0000

    pre allocate

--- dir2data.m
+++ dir2data.m
@@ -1,6 +1,14 @@
 D=dir('*.mat');
 RGB=dir('*.jpg');
+
 [n,~]=size(D);
+
+file_name=zeros(n);
+depth_cube=zeros(480,640,n);
+r_cube=zeros(480,640,n);
+g_cube=zeros(480,640,n);
+b_cube=zeros(480,640,n);
+
 for i=1:n
     file_name(i)=struct('rgb',RGB(i).name,'depth',D(i).name);
     depth_mat=load(file_name(i).depth);
@@ -10,7 +18,11 @@ for i=1:n
     g_cube(:,:,i)=imgRGB(:,:,2);
     b_cube(:,:,i)=imgRGB(:,:,3);
 end
+
 [x,y,~]=size(depth_cube);
+rbg_back=zeros(x,y,3);
+depth_back=zeros(x,y);
+
 for i=1:x
     for j=1:y
         depth_back(i,j)=median(depth_cube(i,j,:));
@@ -19,5 +31,6 @@ for i=1:x
         rbg_back(i,j,3)=median(b_cube(i,j,:));
     end
 end
+
 imagesc(depth_back);
 imshow(rbg_back);
