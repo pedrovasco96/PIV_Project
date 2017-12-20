8870d4038ecdb28551f3f564c772e14a5c8180e0
Marco Graça <marcostbgr16@gmail.com>
2017-12-20 13:56:13 +0000

    struct of tracked objects in testing phase

--- feat_detect.m
+++ feat_detect.m
@@ -1,35 +1,76 @@
-function [feat_d,foreg]=feat_detect(BG1,BG2,imseq1,imseq2,x,y,n,cam_params,subfolder)
+function [feat_d,foreg,matches]=feat_detect(BG1,BG2,R12,T12,imseq1,imseq2,x,y,n,cam_params,subfolder)
 feat_d=struct('frame_cam_1',cell(n,1),'desc_cam_1',cell(n,1),'frame_cam_2',cell(n,1),'desc_cam_2',cell(n,1));
-foreg=struct('foreground_cam_1',cell(n,1),'foreground_cam_2',cell(n,1),'obj_cam_1',cell(n,1),'obj_cam_2',cell(n,1));
-figure();
+foreg=struct('foreground_cam_1',cell(n,1),'foreground_cam_2',cell(n,1),'objs2to1',cell(n,1));
+bg1=reshape(BG1,[x y]);
+bg2=reshape(BG2,[x y]);
+matches=0;
+% figure();
 for i=1:n
     dep1=load(strcat(subfolder,'/',imseq1(i).depth));
-    dep1=single(dep1.depth_array(:));
-    rgb1=imread(strcat(subfolder,'/',imseq1(i).rgb));
-    xyz1=get_xyzasus(dep1,[x y],find(dep1),cam_params.Kdepth,1,0);
-    rgbd1=get_rgbd(xyz1,rgb1,cam_params.R,cam_params.T,cam_params.Krgb);
+    dep1=single(dep1.depth_array);
+    Dep1=imgradient(dep1)./8;
     dep2=load(strcat(subfolder,'/',imseq2(i).depth));
-    dep2=single(dep2.depth_array(:));
+    dep2=single(dep2.depth_array);
+    Dep2=imgradient(dep2)./8;
+    rgb1=imread(strcat(subfolder,'/',imseq1(i).rgb));
     rgb2=imread(strcat(subfolder,'/',imseq2(i).rgb));
-    xyz2=get_xyzasus(dep2,[x y],find(dep2),cam_params.Kdepth,1,0);
+    xyz1=get_xyzasus(dep1(:),[x y],find(dep1),cam_params.Kdepth,1,0);
+    xyz2=get_xyzasus(dep2(:),[x y],find(dep2),cam_params.Kdepth,1,0);
+    rgbd1=get_rgbd(xyz1,rgb1,cam_params.R,cam_params.T,cam_params.Krgb);
     rgbd2=get_rgbd(xyz2,rgb2,cam_params.R,cam_params.T,cam_params.Krgb);
-    fg1=imfill(imopen(abs(dep1-BG1.*single(logical(dep1)))>100,strel('disk',3)),'holes');
-    fg2=imfill(imopen(abs(dep2-BG2.*single(logical(dep2)))>100,strel('disk',3)),'holes');
-    labels1=squeeze(bwlabel(fg1,8));
-%     n1=max(labels1);
-    labels2=squeeze(bwlabel(fg2,8));
-    foreg(i).foreground_cam_1=cat(2,xyz1(fg1,:),labels1(fg1));
-    foreg(i).foreground_cam_2=cat(2,xyz2(fg2,:),labels2(fg2));
-    img1=rgb2gray(rgbd1).*uint8(reshape(fg1,[x,y]));
-    img2=rgb2gray(rgbd2).*uint8(reshape(fg2,[x,y]));
-    imshow(img1);
-    [feat_e1,desc1] = vl_sift(single(img1),'PeakThresh',10);
-    [feat_e2,desc2] = vl_sift(single(img2),'PeakThresh',10);
-    h1 = vl_plotframe(feat_e1);
-    set(h1,'color','y','linewidth',2);
-    feat_d(i).frame_cam_1 = feat_e1;
-    feat_d(i).frame_cam_2 = feat_e2;
+    fg1=bwareaopen(imfill(imopen(abs(dep1-bg1)>200,strel('disk',3)),'holes'),200);
+    fg2=bwareaopen(imfill(imopen(abs(dep2-bg2)>200,strel('disk',3)),'holes'),200);
+    fg1(Dep1>200)=0;
+    fg2(Dep2>200)=0;
+%     imshow(fg1);
+    img1=rgb2gray(rgbd1).*uint8(fg1);
+    img2=rgb2gray(rgbd2).*uint8(fg2);
+    [feat_e1,desc1] = vl_sift(single(img1));
+    [feat_e2,desc2] = vl_sift(single(img2));
+%     h1 = vl_plotframe(feat_e2);
+%     set(h1,'color','y','linewidth',2);
+    feat_e1(1:2,:)=flip(feat_e1(1:2,:),1);
+    feat_e2(1:2,:)=flip(feat_e2(1:2,:),1);
+    val_f1=fg1(sub2ind([x y],round(feat_e1(1,:)),round(feat_e1(2,:))));
+    val_f2=fg2(sub2ind([x y],round(feat_e2(1,:)),round(feat_e2(2,:))));
+    feat_e1=feat_e1(:,val_f1);
+    desc1=desc1(:,val_f1);
+    feat_e2=feat_e2(:,val_f2);
+    desc2=desc2(:,val_f2);
+    ConComp1=bwconncomp(bwselect(fg1,feat_e1(2,:),feat_e1(1,:),8));
+    ConComp2=bwconncomp(bwselect(fg2,feat_e2(2,:),feat_e2(1,:),8));
+    objs1=single(labelmatrix(ConComp1));
+    objs2=single(labelmatrix(ConComp2));
+    feat_d(i).frame_cam_1 = [feat_e1;objs1(sub2ind([x y],round(feat_e1(1,:)),round(feat_e1(2,:))))];
+    feat_d(i).frame_cam_2 = [feat_e2;objs2(sub2ind([x y],round(feat_e2(1,:)),round(feat_e2(2,:))))];
     feat_d(i).desc_cam_1 = desc1;
     feat_d(i).desc_cam_2 = desc2;
-    pause(.05);
+    foreg(i).foreground_cam_1=cell(ConComp1.NumObjects,1);
+    foreg(i).foreground_cam_2=cell(ConComp2.NumObjects,1);
+    centr1=zeros(ConComp1.NumObjects,3);
+    for j=1:ConComp1.NumObjects
+        A=cell2mat(ConComp1.PixelIdxList(1,j));
+        foreg(i).foreground_cam_1{j}=[max(xyz1(A,:),[],1),min(xyz1(A,:),[],1)];
+        centr1(j,:)=mean(xyz1(A,:));
+    end
+    for j=1:ConComp2.NumObjects
+        A=cell2mat(ConComp2.PixelIdxList(1,j));
+        foreg(i).foreground_cam_2{j}=[max(xyz2(A,:),[],1),min(xyz2(A,:),[],1)];
+        centr2=mean(xyz2(A,:))';
+        centr2=R12*centr2+T12';
+        dist2cent=zeros(1,ConComp1.NumObjects);
+        for k=1:ConComp1.NumObjects
+            dist2cent(1,k)=norm(centr2-centr1(k,:)');
+        end
+        dist2cent(dist2cent>0.5)=NaN;
+        [isattr,centr2in1]=min(dist2cent);
+        if ~isnan(isattr)
+            matches=matches+1;
+            foreg(i).objs2to1(j)=centr2in1;
+        else
+            foreg(i).objs2to1(j)=0;
+        end
+    end
+%     imagesc([objs1 objs2]);
+%     pause(.05);
 end
--- feat_match.m
+++ feat_match.m
@@ -1,9 +1,51 @@
-function feat_match(imseq1,imseq2,feat_d,foreg,cam_params,x,y,subfolder,img)
-[R12,T12]=getfusedPC(imseq1(img),imseq2(img),cam_params,x,y,subfolder);
+function feat_match(feat_d,foreg)
 sz=numel(feat_d)-1;
-match=struct('feat1',cell(sz,1),'sc1',cell(sz,1),'feat2',cell(sz,1),'sc2',cell(sz,1));
-for i=1:numel(feat_d)-1
-[match(i).feat1, match(i).sc1] = vl_ubcmatch(feat_d(i).desc_cam_1, feat_d(i+1).desc_cam_1) ;
-[match(i).feat2, match(i).sc2] = vl_ubcmatch(feat_d(i).desc_cam_2, feat_d(i+1).desc_cam_2) ;
+trackedobj=cell(sz+1,2);
+for i=1:sz
+    matchfeat1 = vl_ubcmatch(feat_d(i).desc_cam_1, feat_d(i+1).desc_cam_1);
+    matchfeat2 = vl_ubcmatch(feat_d(i).desc_cam_2, feat_d(i+1).desc_cam_2);
+    datacam2now=nonzeros(foreg(i).objs2to1(feat_d(i).frame_cam_2(5,matchfeat2(1,:))));
+    if ~isempty(datacam2now)
+        ft_objs_now=[feat_d(i).frame_cam_1(5,matchfeat1(1,:)),datacam2now];
+    else
+        ft_objs_now=feat_d(i).frame_cam_1(5,matchfeat1(1,:));
+    end
+    datacam2next=nonzeros(foreg(i+1).objs2to1(feat_d(i+1).frame_cam_2(5,matchfeat2(2,:))));
+    if ~isempty(datacam2next)
+        ft_objs_next=[feat_d(i+1).frame_cam_1(5,matchfeat1(2,:)),datacam2next];
+    else
+        ft_objs_next=feat_d(i+1).frame_cam_1(5,matchfeat1(2,:));
+    end
+    matched_objs=unique(ft_objs_next);
+    matching_objs=zeros(1,numel(matched_objs));
+    for j=1:numel(matched_objs)
+        column=zeros(size(matched_objs));
+        for k=1:numel(column)
+            column(k)=sum(ismember(ft_objs_next,matched_objs(k)));
+        end
+        matching_objs(j)=mode(column);
+    end
+    [dup,freq]=mode(matching_objs);
+    while freq > 1 && dup ~= 0
+        matching_objs(matching_objs==dup)=0;
+        [dup,freq]=mode(nonzeros(matching_objs));
+    end
+    if i==1
+        num_tr_objs=numel(unique(ft_objs_now));
+        trackedobj{i,1}=1:numel(num_tr_objs);
+        trackedobj{i,2}=unique(ft_objs_now);
+    else
+        newobjs=setdiff(ft_objs_now,trackedobj{i,2});
+        trackedobj{i,1}=[trackedobj{i,1},num_tr_objs:(num_tr_objs+numel(newobjs))];
+        trackedobj{i,2}=[trackedobj{i,2},newobjs];
+        num_tr_objs=num_tr_objs+numel(newobjs);
+    end
+    tr_obj_num=trackedobj{i,1};
+    obj_in_frame=trackedobj{i,2};
+    [aux1,aux2]=ismember(matching_objs,obj_in_frame);
+    nobjs=ismember(matching_objs,0);
+    trackedobj{i+1,1}=[tr_obj_num(nonzeros(aux2)),num_tr_objs:num_tr_objs+sum(nobjs)];
+    trackedobj{i+1,2}=[matched_objs(aux1),matched_objs(nobjs)];
+    num_tr_objs=num_tr_objs+sum(nobjs);
 end
 end
